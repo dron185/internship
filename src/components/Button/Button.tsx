0a73498b665164a5699749e43513f24416086640
Andrei Huryn <gurin.nord@gmail.com>
2024-11-27 16:48:15 +0300

    feat: refactor Button add Slot to implement polymorphic component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,18 @@
 import {ComponentPropsWithoutRef} from "react";
 import s from "./Button.module.css"
 import clsx from "clsx";
+import {Slot} from "@radix-ui/react-slot";
 
 type Props = {
     variant?: 'primary' | 'secondary' | 'outlined'
+    asChild?: boolean
 } & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({ variant = 'primary', className, ...rest }: Props) => {
+export const Button = ({ variant = 'primary', className, asChild,  ...rest }: Props) => {
+
+    const Component = asChild ? Slot : 'button'
+
     return (
-        <button className={clsx(s.button, s[variant], className)} {...rest}/>
+        <Component className={clsx(s.button, s[variant], className)} {...rest}/>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 import {Cart} from "../Cart/Cart.tsx";
 import s from './Header.module.css'
-import {ModalRadix} from "../Modal/ModalRadix.tsx";
+import {Button} from "../Button/Button.tsx";
 
 export const Header = () => {
     return (
         <div className={s.headerWrapper}>
             <div className={s.container}>
                 <h3>logotype</h3>
+                <Button asChild>
+                    <a href="/sign-up">Sign Up</a>
+                </Button>
                 <Cart />
-                <ModalRadix />
             </div>
         </div>
     )
